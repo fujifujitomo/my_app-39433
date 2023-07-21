101292d9f9c77aaf67e9a9a858e0f38c05a43608
fujifujitomo <futomo0421@gmail.com>
2023-07-21 23:00:50 +0900

    追加、修正

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,30 +1,12 @@
 class PetsController < ApplicationController
 
-  def index
-    @pets = Pet.all
-  end
-
   def new
     @pet = Pet.new
   end
 
-  def create
-    @pet = Pet.new(pet_params)
-    if @pet.save
-      redirect_to root_path(@pet)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
   def show
-    @pet = Pet.new
-  end
-
-  private
-
-  def pet_params
-    params.require(:pet).permit(:name, :gender, :kind, :birthday, :image).merge(user_id: current_user.id)
+    @pets = Pet.find(params[:id])
+    @user = @pet.user
   end
 
 end
